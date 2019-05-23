38a48468f6a5b3636dabaed5c367e4d4c2188bca
Leandro <leaxviana140@gmail.com>
2019-05-23 17:09:46 -0400

    remove libs

---
+++ src/PacotePrincipal/JFrame_Main.java
@@ -0,0 +1,22 @@
+package PacotePrincipal;
+
+
+import javax.swing.JFrame;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author pixel
+ */
+public class JFrame_Main extends JFrame {
+
+    public static void main(String[] args) {
+
+    }
+
+}
