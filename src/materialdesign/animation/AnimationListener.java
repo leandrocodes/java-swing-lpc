e274c15939730f0ddbed02faba3717e7938ec166
Leandro <leaxviana140@gmail.com>
2019-05-23 16:26:56 -0400

    material ui lib

---
+++ src/materialdesign/animation/AnimationListener.java
@@ -0,0 +1,12 @@
+package materialdesign.animation;
+
+public interface AnimationListener {
+
+    public void onStart();
+
+    public void onAnimation(double percent);
+
+    public void onStop();
+
+    public void onEnd();
+}
